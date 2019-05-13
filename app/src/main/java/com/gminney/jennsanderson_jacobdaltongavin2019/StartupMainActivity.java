6354776ec229e7886cc8b9f70d74cce46f878ad9
Jborder2019 <31282171+Jborder2019@users.noreply.github.com>
2019-05-13 13:12:48 -0400

    Eigth Committ - 5/13 1:12

--- app/src/main/java/com/gminney/jennsanderson_jacobdaltongavin2019/StartupMainActivity.java
+++ app/src/main/java/com/gminney/jennsanderson_jacobdaltongavin2019/StartupMainActivity.java
@@ -2,6 +2,7 @@ package com.gminney.jennsanderson_jacobdaltongavin2019;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Switch;
 
 import net.dv8tion.jda.api.AccountType;
 import net.dv8tion.jda.api.JDABuilder;
@@ -10,15 +11,15 @@ import javax.security.auth.login.LoginException;
 
 public class StartupMainActivity extends AppCompatActivity {
 
+    private Switch switch1;
+
+
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) throws LoginException {
+    protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.startup_activity_main);
-
-        JDABuilder bot = new JDABuilder(AccountType.BOT);
-        bot.setToken(getString(R.string.TOKEN_STRING));
-        bot.build();
-
+        switch1 = findViewById(R.id.switch1);
 
 
 
