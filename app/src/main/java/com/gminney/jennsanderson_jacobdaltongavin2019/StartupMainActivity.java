27dbfbe0bd81266b11107a355713c1b235851350
Jborder2019 <31282171+Jborder2019@users.noreply.github.com>
2019-05-21 10:38:18 -0400

    5/21/19 10:38 - GMan Push

--- app/src/main/java/com/gminney/jennsanderson_jacobdaltongavin2019/StartupMainActivity.java
+++ app/src/main/java/com/gminney/jennsanderson_jacobdaltongavin2019/StartupMainActivity.java
@@ -1,19 +1,19 @@
 package com.gminney.jennsanderson_jacobdaltongavin2019;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Switch;
 
-import net.dv8tion.jda.api.AccountType;
-import net.dv8tion.jda.api.JDABuilder;
-
-import javax.security.auth.login.LoginException;
 
 public class StartupMainActivity extends AppCompatActivity {
 
     private Switch switch1;
     private Button button1;
+    private EditText mEditText;
 
 
 
@@ -22,11 +22,20 @@ public class StartupMainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         switch1 = findViewById(R.id.switch1);
         button1 = findViewById(R.id.button1);
+        mEditText = findViewById(R.id.editText2);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mEditText.getText().toString() == getString(R.string.password)){
+                    Intent intent = new Intent(StartupMainActivity.this, BotStatusActivity.this);
+                    startActivity(intent);
+                }
+            }
+        });
 
 
 
     }
-
-
 
 }
