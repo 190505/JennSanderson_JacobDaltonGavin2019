85f72cb2cd87cad818a0400dda4c36db510f4719
190505 <lloydminn@gmail.com>
2019-05-23 10:40:01 -0400

    10:39 5/23

---
+++ app/src/main/java/com/gminney/jennsanderson_jacobdaltongavin2019/BotStatusActivity.java
@@ -0,0 +1,71 @@
+package com.gminney.jennsanderson_jacobdaltongavin2019;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class BotStatusActivity extends AppCompatActivity {
+
+    private static Socket socket;
+    private static ServerSocket serverSocket;
+    private static InputStreamReader inputStreamReader;
+    private static BufferedReader bufferedReader;
+    private static PrintWriter printWriter;
+    private String message;
+    private static String ip = "172.20.10.147" ;
+
+    private EditText urlTextField1;
+    private EditText urlTextField2;
+    private EditText urlTextField3;
+    private EditText urlTextField4;
+    private EditText urlTextField5;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.bot_status_activity);
+
+        urlTextField1 = findViewById(R.id.urlText1);
+        urlTextField2 = findViewById(R.id.urlText2);
+        urlTextField3 = findViewById(R.id.urlText3);
+        urlTextField4 = findViewById(R.id.urlText4);
+        urlTextField5 = findViewById(R.id.urlText5);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("Send button -  message sent");
+                message = urlTextField1.getText().toString().trim();
+                try {
+                    socket = new Socket(ip, 5000);
+                    printWriter = new PrintWriter(socket.getOutputStream());
+                    printWriter.write(message);
+                    printWriter.flush();
+                    printWriter.close();
+                    socket.close();
+                }
+                catch (IOException exception){
+
+                }
+
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/gminney/jennsanderson_jacobdaltongavin2019/StartupMainActivity.java
+++ app/src/main/java/com/gminney/jennsanderson_jacobdaltongavin2019/StartupMainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Switch;
@@ -15,11 +17,12 @@ public class StartupMainActivity extends AppCompatActivity {
     private Button button1;
     private EditText mEditText;
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState)  {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.startup_activity_main);
         switch1 = findViewById(R.id.switch1);
         button1 = findViewById(R.id.button1);
         mEditText = findViewById(R.id.editText2);
@@ -27,9 +30,10 @@ public class StartupMainActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (mEditText.getText().toString() == getString(R.string.password)){
-                    Intent intent = new Intent(StartupMainActivity.this, BotStatusActivity.this);
+                if (mEditText.getText().toString().trim().equals(getString(R.string.password))){
+                    Intent intent = new Intent(getApplicationContext(), BotStatusActivity.class);
                     startActivity(intent);
+                    System.out.println("Correct Password");
                 }
             }
         });
